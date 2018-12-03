4cd83008eddb30a4b1accd82259afce2cb46e5b9
Mturbeville <44788701+Mturbeville@users.noreply.github.com>
2018-12-03 13:34:18 -0500

    Add files via upload

--- FinalProject/form.php
+++ FinalProject/form.php
@@ -103,7 +103,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             );
             $statement->execute($params);
         };	
-		header('location: index.php');
+		header('location: admin.php');
 	}
 	
 }
